f96a75261e19246ad6dd9d23a3615db6b5ae8047
mads2ez <maximsivtsev@gmail.com>
2016-11-23 01:50:25 +0900

    added removeAlarm() and check for tomorrow alarms

--- js/script.js
+++ js/script.js
@@ -1,29 +1,52 @@
+// debug
 var say = function(q) {
   console.log(q);
 };
 
-var audio = new Audio('sound/surroundings.mp3');
-
+// caсhe html
 var	$time = document.getElementById('time'),
 		$date = document.getElementById('date'),
 		$addAlarmBtn = document.getElementById('addalarm'),
 		$alarmsList = document.getElementById('alarms'),
 		$alarmForm = document.getElementById('alarmform'),
 		$alarmModal = document.querySelector('.alarm_modal');
-		timer = null;
 
-var alarms = [];
+function showModal() {
+	$alarmModal.classList.toggle('active');
+}
 
-function fireAlarm() {
+// execute alarm
+var audio = new Audio('sound/surroundings.mp3');
+
+var fireAlarm = function() {
 	audio.play();
 };
 
-function checkAlarms() {
-	var today = new Date();
-	var now = padfield(today.getHours()) + ":" + padfield(today.getMinutes());
-	alarms.forEach(function(alarm) {
+// set and remove alarms
+var alarms = [];
 
-			var html = '<li class="alarm">' + alarm +
+var setAlarms = function() {
+	// var today = new Date();
+	// var h = today.getHours();
+  // var m = today.getMinutes();
+  // var s = today.getSeconds();
+  <!-- -->
+  var date = new Date(), year = date.getFullYear(), month = date.getMonth(), day = parseInt( date.getDate() );
+  <!-- -->
+	for (i = 0; i < alarms.length; i++)	{
+    <!-- -->
+    var tomo = false;
+    if( alarms[i].split(':')[0] < date.getHours() )
+      {tomo = true;}
+    else if( alarms[i].split(':')[0] == date.getHours() && alarms[i].split(':')[1] < date.getMinutes() )
+      {tomo = true;}
+    else if( alarms[i].split(':')[0] == date.getHours() && alarms[i].split(':')[1] == date.getMinutes() && 0 < date.getSeconds() )
+      {tomo = true;}
+    if( tomo ){day += 1;}
+    say(tomo);
+    <!-- -->
+			var html = '<li class="alarm">' + alarms[i] +
+                  '<a href="#" onClick="removeAlarm(' + i + ');">' +
 										'<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"' +
 											 'width="612px" height="612px" viewBox="0 0 612 612" style="enable-background:new 0 0 612 612;" xml:space="preserve">' +
 													'<path id="del" d="M387.128,170.748L306,251.915l-81.128-81.167l-54.124,54.124L251.915,306l-81.128,81.128l54.085,54.086L306,360.086' +
@@ -32,17 +55,67 @@ function checkAlarms() {
 														'c119.493,119.493,313.267,119.493,432.76,0C641.873,402.888,641.873,209.113,522.38,89.62z M468.295,468.295' +
 														'c-89.62,89.619-234.932,89.619-324.551,0c-89.62-89.62-89.62-234.932,0-324.551c89.62-89.62,234.931-89.62,324.551,0' +
 														'C557.914,233.363,557.914,378.637,468.295,468.295z"/>' +
-											'</svg>' +
+											'</svg></a>' +
 									'</li>';
-			$alarmsList.innerHTML += html;
-			timer = ((Number(alarm.split(':')[0])*60+Number(alarm.split(':')[1]))*60000) - ((Number(now.split(':')[0])*60+Number(now.split(':')[1]))*60000);
-
-		setTimeout(function() {
-			fireAlarm(alarm);
-		}, timer);
-	})
+			// var refreshTime = ((Number(alarms[i].split(':')[0])*3600+Number(alarms[i].split(':')[1])*60)*1000) - ((h*3600+m*60+s)*1000);
+      <!-- -->
+      var alarmTime = new Date( year, month, day, alarms[i].split(':')[0], alarms[i].split(':')[1], 0, date.getMilliseconds() );
+      alarmTime = Math.abs(alarmTime - new Date());
+      say(alarmTime);
+      <!-- -->
+		  setTimeout(function() {fireAlarm(alarms[i])}, alarmTime);
+	};
+  $alarmsList.innerHTML += html;
 };
 
+function removeAlarm(j) {
+  // var today = new Date();
+  // var h = today.getHours();
+  // var m = today.getMinutes();
+  // var s = today.getSeconds();
+  <!-- -->
+  var date = new Date(), year = date.getFullYear(), month = date.getMonth(), day = parseInt( date.getDate() );
+  <!-- -->
+  alarms.splice(j, 1);
+  var refreshAlarm = '';
+  for (i = 0; i < alarms.length; i++)	{
+    <!-- -->
+    var tomo = false;
+    if( alarms[i].split(':')[0] < date.getHours() )
+      {tomo = true;}
+    else if( alarms[i].split(':')[0] == date.getHours() && alarms[i].split(':')[1] < date.getMinutes() )
+      {tomo = true;}
+    else if( alarms[i].split(':')[0] == date.getHours() && alarms[i].split(':')[1] == date.getMinutes() && 0 < date.getSeconds() )
+      {tomo = true;}
+    if( tomo ){day += 1;}
+    <!-- -->
+      var html = '<li class="alarm">' + alarms[i] +
+                  '<a href="#" onClick="removeAlarm(' + i + ');">' +
+                    '<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"' +
+                       'width="612px" height="612px" viewBox="0 0 612 612" style="enable-background:new 0 0 612 612;" xml:space="preserve">' +
+                          '<path id="del" d="M387.128,170.748L306,251.915l-81.128-81.167l-54.124,54.124L251.915,306l-81.128,81.128l54.085,54.086L306,360.086' +
+                            'l81.128,81.128l54.086-54.086L360.086,306l81.128-81.128L387.128,170.748z M522.38,89.62' +
+                            'c-119.493-119.493-313.267-119.493-432.76,0c-119.493,119.493-119.493,313.267,0,432.76' +
+                            'c119.493,119.493,313.267,119.493,432.76,0C641.873,402.888,641.873,209.113,522.38,89.62z M468.295,468.295' +
+                            'c-89.62,89.619-234.932,89.619-324.551,0c-89.62-89.62-89.62-234.932,0-324.551c89.62-89.62,234.931-89.62,324.551,0' +
+                            'C557.914,233.363,557.914,378.637,468.295,468.295z"/>' +
+                      '</svg></a>' +
+                  '</li>';
+      refreshAlarm += html;
+      // var refreshTime = ((Number(alarms[i].split(':')[0])*3600+Number(alarms[i].split(':')[1])*60)*1000) - ((h*3600+m*60+s)*1000);
+      <!-- -->
+      var alarmTime = new Date( year, month, day, alarms[i].split(':')[0], alarms[i].split(':')[1], 0, date.getMilliseconds() );
+      alarmTime = Math.abs(alarmTime - new Date());
+      say(alarmTime);
+      <!-- -->
+      setTimeout(function() {fireAlarm(alarms[i])}, alarmTime);
+  };
+  $alarmsList.innerHTML = refreshAlarm;
+  audio.pause();
+  audio.currentTime = 0;
+};
+
+// time and date convertation
 var padfield = function(f) {
 	return (f<10)? "0"+f : f;
 };
@@ -73,10 +146,11 @@ var day = function(day) {
 			return day;
 		default:
 			day = 'undefined';
-			return day;
+			break;
 	}
 };
 
+// show current time
 var showCurrentTime = function() {
   var today = new Date();
   var s = today.getSeconds();
@@ -87,24 +161,19 @@ var showCurrentTime = function() {
 	$date.innerHTML = d + " " + day(w);
 };
 
-function showModal() {
-	$alarmModal.classList.toggle('active');
-};
-
+// event handlers
 $addAlarmBtn.onclick = function() {
 	showModal();
 };
 
 $alarmForm.addEventListener('submit', function(e) {
-	e.preventDefault()
-  var alarmTime = document.getElementById('alarm_time').value;
-	if (alarm_time !== "") {
-		alarms.push(alarmTime);
-		checkAlarms();
+	e.preventDefault();
+  var alarm = document.getElementById('alarm_time').value;
+	if (alarm_time != "") {
+		alarms.push(alarm);
+		setAlarms();
 		showModal();
 	}
 });
-
-
 
 setInterval(function() {showCurrentTime()}, 1000);
